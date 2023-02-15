2b61240ff0d1e451a39da03b89c6d05653477865
narissarah <narissarahoing@icloud.com>
2023-02-15 16:34:52 -0500

    Initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+export default {
+	themeConfig: {
+	  logo: '/logo.png',
+	  siteTitle: 'narissara.lol',
+	  nav: [
+		{ text: 'About', link: '/about' },
+		{ text: 'Work', link: '/work' },
+		{ text: 'Blog', link: '/blog' },
+	  ],
+	  socialLinks: [
+		{ icon: 'github', link: 'https://github.com/narissarah/narissarah.github.io' },
+	  ]
+	}
+  }
+  
